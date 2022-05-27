71724a995fb7063fe723d0135476ea805f1441eb
Shaad Alaka <shaad1@live.nl>
2022-05-27 08:46:14 +0200

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,301 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> ML Hub </q-toolbar-title>
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      class="bg-white"
+      :width="400"
+      :breakpoint="200"
+    >
+      <q-list>
+        <p class="q-pa-lg q-mb-none text-h6 text-uppercase">Search Filters</p>
+        <q-item
+          v-for="(val, index) in attributes"
+          :key="val.label"
+          dense
+          clickable
+        >
+          <q-expansion-item
+            ref="expanders"
+            icon="square"
+            :id="val"
+            @show="this.closeExpanders(index, $event)"
+            :expand-icon-class="getColor(val.label)"
+            v-model="attributeExpanderStates[val.label]"
+            style="width: 100%"
+            dense
+            :label="val.label"
+          >
+            <template v-slot:header>
+              <q-item-section avatar>
+                <q-avatar icon="square" :text-color="getColor(val.label)" />
+              </q-item-section>
+
+              <q-item-section> {{ val.label }} </q-item-section>
+            </template>
+
+            <q-card v-if="val.type == 'string'">
+              <q-card-section>
+                <q-form
+                  @submit="
+                    this.closeExpanders();
+                    this.submitFilter(val.label);
+                  "
+                >
+                  <q-input
+                    filled
+                    v-model="attributeModels[val.label]"
+                    label="Name"
+                  />
+                </q-form>
+              </q-card-section>
+            </q-card>
+
+            <q-card v-if="val.type == 'categorical'">
+              <q-card-section>
+                <q-form ref="form">
+                  <q-select
+                    filled
+                    @update:model-value="
+                      this.closeExpanders();
+                      this.submitFilter(val.label);
+                    "
+                    v-model="attributeModels[val.label]"
+                    :options="val.categories"
+                    label="Type"
+                  />
+                </q-form>
+              </q-card-section>
+            </q-card>
+
+            <!-- TODO: allow custom input -->
+            <q-card v-if="val.type == 'metric'">
+              <q-card-section>
+                <q-form
+                  @submit="
+                    this.closeExpanders();
+                    this.submitFilter(val.label);
+                  "
+                >
+                  <q-select
+                    filled
+                    v-model="attributeModels[val.label]"
+                    :options="val.metrics"
+                    label="Metric"
+                  />
+                  <q-input
+                    filled
+                    type="number"
+                    step="0.1"
+                    v-model="attributeModelValues[val.label]"
+                    label="Value"
+                  />
+                  <q-btn v-show="false" label="Submit" type="submit"></q-btn>
+                </q-form>
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+        </q-item>
+      </q-list>
+      <q-separator></q-separator>
+      <p class="q-pa-lg q-mb-none text-h6 text-uppercase">Selected Filters</p>
+      <q-chip
+        removable
+        v-for="val in this.activeFilters"
+        :key="val.keyword"
+        :color="getColor(val.attribute)"
+        @remove="this.removeFilter(val)"
+        square
+      >
+        {{ val.keyword }}
+        <q-badge v-if="val.value" color="grey"> {{ val.value }} </q-badge>
+      </q-chip>
+    </q-drawer>
+    <q-page-container>
+      <IndexPage
+        ref="results"
+        :filters="this.activeFilters"
+        :models="this.models"
+      />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import IndexPage from "pages/IndexPage.vue";
+import loki from "lokijs";
+import * as jsonData from "../assets/dump.json";
+import { nextTick } from "vue";
+const attrList = [
+  {
+    color: "purple-3",
+    label: "Training Data",
+    type: "string",
+  },
+  {
+    color: "teal-3",
+    label: "Type",
+    type: "categorical",
+    categories: [
+      { label: "Audio" },
+      { label: "Computer Vision" },
+      { label: "Multimodal" },
+      { label: "Natural Language Processing" },
+      { label: "Reinforcement Learning" },
+      { label: "Structured" },
+    ],
+  },
+  {
+    color: "lime-3",
+    label: "Task",
+    type: "categorical",
+    categories: [
+      { label: "Audio Classification" },
+      { label: "Audio To Audio" },
+      { label: "Automatic Speech Recognition" },
+      { label: "Conversational" },
+      { label: "Feature Extraction" },
+      { label: "Fill Mask" },
+      { label: "Image Classification" },
+      { label: "Image Segmentation" },
+      { label: "Image To Image" },
+      { label: "Image To Text" },
+      { label: "Object Detection" },
+      { label: "Question Answering" },
+      { label: "Reinforcement Learning" },
+      { label: "Sentence Similarity" },
+      { label: "Summarization" },
+      { label: "Table Question Answering" },
+      { label: "Tabular Classification" },
+      { label: "Text Classification" },
+      { label: "Text Generation" },
+      { label: "Text To Image" },
+      { label: "Text To Speech" },
+      { label: "Text2text Generation" },
+      { label: "Token Classification" },
+      { label: "Translation" },
+      { label: "Unconditional Image Generation" },
+      { label: "Voice Activity Detection" },
+      { label: "Zero Shot Classification" },
+      { label: "Zero Shot Image Classification" },
+    ],
+  },
+  {
+    color: "brown-3",
+    label: "Hyperparameters",
+    type: "metric",
+    metrics: [
+      { label: "learning_rate", type: "number" },
+      { label: "batch_size", type: "number" },
+      { label: "seed", type: "number" },
+      { label: "total_train_batch_size", type: "number" },
+      { label: "optimizer", type: "string" },
+      { label: "num_epochs", type: "number" },
+    ],
+  },
+  {
+    color: "indigo-3",
+    label: "Evaluation Results",
+    type: "metric",
+    metrics: [
+      { label: "accuracy", type: "number" },
+      { label: "precision", type: "number" },
+      { label: "recall", type: "number" },
+      { label: "f1", type: "number" },
+      { label: "generic", type: "number" },
+    ],
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    IndexPage,
+  },
+  data() {
+    return {
+      attributeModels: Object.assign(
+        {},
+        ...attrList.map((x) => {
+          return { [x.label]: null };
+        })
+      ),
+      attributeModelValues: Object.assign(
+        {},
+        ...attrList.map((x) => {
+          return { [x.label]: null };
+        })
+      ),
+      attributeExpanderStates: attrList.map((x) => {
+        return { [x.label]: false };
+      }),
+      activeFilters: [],
+    };
+  },
+  computed: {
+    console: () => console,
+  },
+  methods: {
+    removeFilter(model) {
+      this.activeFilters = this.activeFilters.filter((x) => x !== model);
+    },
+    getColor(attr) {
+      var color;
+      this.attributes.forEach((element) => {
+        if (element.label == attr) {
+          color = element.color;
+        }
+      });
+      return color;
+    },
+    closeExpanders(index, e) {
+      this.$refs.expanders.forEach((x, i) => {
+        if (i != index) {
+          x.hide();
+        }
+      });
+    },
+    submitFilter(attr) {
+      var context = this;
+      nextTick(function () {
+        context.activeFilters.push({
+          attribute: attr,
+          keyword:
+            context.attributeModels[attr].label == null
+              ? context.attributeModels[attr]
+              : context.attributeModels[attr].label,
+          value: context.attributeModelValues[attr],
+        });
+        context.attributeModels[attr] = null;
+        context.attributeModelValues[attr] = null;
+      });
+    },
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+    var db = new loki("mlhub.db");
+    var models = db.addCollection("models");
+    // The JSON is copied because Vue automatically turns it into a property, interfering with LokiJS
+    models.insert(JSON.parse(JSON.stringify(jsonData))["default"]);
+    return {
+      models: models,
+      original: attrList,
+      attributes: attrList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,121 @@
+<template>
+  <q-page style="background: `grey`">
+    <q-form @submit="this.search">
+      <q-input color="primary" filled v-model="query" label="Model Name">
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    </q-form>
+    <q-list bordered separator>
+      <q-item v-for="obj in this.results" :key="obj">
+        <q-item-section>
+          <q-expansion-item
+            :label="obj[`Model Name`]"
+            :caption="obj[`Task`] + ` --- ` + obj[`Training Data`].join(`, `)"
+            expand-separator
+          >
+            <q-markup-table>
+              <thead>
+                <tr>
+                  <th class="text-left">Property</th>
+                  <th class="text-right">Value</th>
+                </tr>
+                <tr v-for="k in Object.keys(obj)" :key="k">
+                  <td class="text-left">{{ k }}</td>
+                  <td class="text-right">{{ obj[k] }}</td>
+                </tr>
+              </thead>
+            </q-markup-table>
+          </q-expansion-item>
+        </q-item-section>
+        <q-item-section side top>
+          <q-btn
+            size="lg"
+            icon="link"
+            color="secondary"
+            flat
+            :href="obj[`Url`]"
+            target="_blank"
+          />
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  props: {
+    filters: null, // Strangely, initializing as array causes a type mismatch
+    models: null,
+  },
+  data() {
+    return {
+      results: [],
+      query: "",
+    };
+  },
+  watch: {
+    filters: {
+      handler(n, o) {
+        this.search();
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    search() {
+      var res = this.models.chain();
+
+      // Name
+      if (this.query.length > 0) {
+        res.find({ "Model Name": { $contains: this.query } });
+      }
+
+      // Data
+      var datasetFilters = this.filters
+        .filter((f) => f.attribute == "Training Data")
+        .map((f) => f.keyword);
+      if (datasetFilters.length > 0) {
+        res.find({ "Training Data": { $contains: datasetFilters } });
+      }
+
+      // Type
+      var typeFilters = this.filters
+        .filter((f) => f.attribute == "Type")
+        .map((f) => f.keyword);
+      if (typeFilters.length > 0) {
+        res.find({ Type: { $in: typeFilters } });
+      }
+
+      // Task
+      var taskFilters = this.filters
+        .filter((f) => f.attribute == "Task")
+        .map((f) => f.keyword);
+      if (taskFilters.length > 0) {
+        res.find({ Task: { $in: taskFilters } });
+      }
+
+      // Evaluation Result
+      var evalFilters = this.filters
+        .filter((f) => f.attribute == "Evaluation Results")
+        .map((f) => {
+          return { name: f.keyword, value: f.value };
+        });
+      if (evalFilters.length > 0) {
+        res.where(function (obj) {
+          return evalFilters.every((p) => {
+            return obj["Evaluation Results"].some((c) => {
+              return c.metric.includes(p.name) && c.value > p.value;
+            });
+          });
+        });
+      }
+      this.results = res.data();
+    },
+  },
+});
+</script>
